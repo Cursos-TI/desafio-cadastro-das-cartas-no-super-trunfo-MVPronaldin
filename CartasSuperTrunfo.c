96bc117b9f8506b838cbb3a2b63be3a6d471e229
MVPronaldin <v14n41nf0@icloud.com>
2025-02-06 23:33:37 +0000

    Cidades A e B

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,7 +24,7 @@ int main() {
         int pontosA01, pontosA02, pontosA03, pontosA04;
 
         printf("Digite o código da primeira cidade do país A (Ex:A01):\n");
-        scanf("%3s", codigoA01);
+        scanf("%4s", codigoA01);
         printf("Digite o nome da primeira cidade do país A:\n");
         scanf("%s", nomeA01);
         printf("Digite a população da primeira cidade do país A:\n");
@@ -36,8 +36,8 @@ int main() {
         printf("Digite o número de pontos turísticos da primeira cidade do país A:\n");
         scanf("%d", &pontosA01);
 
-        printf("Digite o código da segunda cidade do país A (Ex:A02):\n");
-        scanf("%3s", codigoA02);
+        printf("\nDigite o código da segunda cidade do país A (Ex:A02):\n");
+        scanf("%4s", codigoA02);
         printf("Digite o nome da segunda cidade do país A:\n");
         scanf("%s", nomeA02);
         printf("Digite a população da segunda cidade do país A:\n");
@@ -49,8 +49,8 @@ int main() {
         printf("Digite o número de pontos turísticos da segunda cidade do país A:\n");
         scanf("%d", &pontosA02);
 
-        printf("Digite o código da terceira cidade do país A (Ex:A02):\n");
-        scanf("%3s", codigoA03);
+        printf("\nDigite o código da terceira cidade do país A (Ex:A02):\n");
+        scanf("%4s", codigoA03);
         printf("Digite o nome da terceira cidade do país A:\n");
         scanf("%s", nomeA03);
         printf("Digite a população da terceira cidade do país A:\n");
@@ -62,6 +62,76 @@ int main() {
         printf("Digite o número de pontos turísticos da terceira cidade do país A:\n");
         scanf("%d", &pontosA03);
 
+        printf("\nDigite o código da quarta cidade do país A (Ex:A02):\n");
+        scanf("%4s", codigoA04);
+        printf("Digite o nome da quarta cidade do país A:\n");
+        scanf("%s", nomeA04);
+        printf("Digite a população da quarta cidade do país A:\n");
+        scanf("%d", &populacaoA04);
+        printf("Digite a área(m2) da quarta cidade do país A:\n");
+        scanf("%f", &areaA04);
+        printf("Digite o pib da quarta cidade do país A:\n");
+        scanf("%f", &pibA04);
+        printf("Digite o número de pontos turísticos da quarta cidade do país A:\n");
+        scanf("%d", &pontosA04);
 
+        char codigoB01[4], codigoB02[4], codigoB03[4], codigoB04[4];
+        char nomeB01[20], nomeB02[20], nomeB03[20], nomeB04[20];
+        int populacaoB01, populacaoB02, populacaoB03, populacaoB04;
+        float areaB01, areaB02, areaB03, areaB04;
+        float pibB01, pibB02, pibB03, pibB04;
+        int pontosB01, pontosB02, pontosB03, pontosB04;
+
+        printf("\nDigite o código da primeira cidade do país B (Ex:A01):\n");
+        scanf("%4s", codigoB01);
+        printf("Digite o nome da primeira cidade do país B:\n");
+        scanf("%s", nomeB01);
+        printf("Digite a população da primeira cidade do país B:\n");
+        scanf("%d", &populacaoB01);
+        printf("Digite a área da primeira cidade do país B:\n");
+        scanf("%f", &areaB01);
+        printf("Digite o pib da primeira cidade do país B:\n");
+        scanf("%f", &pibB01);
+        printf("Digite o número de pontos turísticos da primeira cidade do país B:\n");
+        scanf("%d", &pontosB01);
+
+        printf("\nDigite o código da segunda cidade do país B (Ex:A02):\n");
+        scanf("%4s", codigoB02);
+        printf("Digite o nome da segunda cidade do país B:\n");
+        scanf("%s", nomeB02);
+        printf("Digite a população da segunda cidade do país B:\n");
+        scanf("%d", &populacaoB02);
+        printf("Digite a área(m2) da segunda cidade do país B:\n");
+        scanf("%f", &areaB02);
+        printf("Digite o pib da segunda cidade do país B:\n");
+        scanf("%f", &pibB02);
+        printf("Digite o número de pontos turísticos da segunda cidade do país B:\n");
+        scanf("%d", &pontosB02);
+
+        printf("\nDigite o código da terceira cidade do país B (Ex:A02):\n");
+        scanf("%4s", codigoB03);
+        printf("Digite o nome da terceira cidade do país B:\n");
+        scanf("%s", nomeB03);
+        printf("Digite a população da terceira cidade do país B:\n");
+        scanf("%d", &populacaoB03);
+        printf("Digite a área(m2) da terceira cidade do país B:\n");
+        scanf("%f", &areaB03);
+        printf("Digite o pib da terceira cidade do país B:\n");
+        scanf("%f", &pibB03);
+        printf("Digite o número de pontos turísticos da terceira cidade do país B:\n");
+        scanf("%d", &pontosB03);
+
+        printf("\nDigite o código da quarta cidade do país B (Ex:A02):\n");
+        scanf("%4s", codigoB04);
+        printf("Digite o nome da quarta cidade do país B:\n");
+        scanf("%s", nomeB04);
+        printf("Digite a população da quarta cidade do país B:\n");
+        scanf("%d", &populacaoB04);
+        printf("Digite a área(m2) da quarta cidade do país B:\n");
+        scanf("%f", &areaB04);
+        printf("Digite o pib da quarta cidade do país B:\n");
+        scanf("%f", &pibB04);
+        printf("Digite o número de pontos turísticos da quarta cidade do país B:\n");
+        scanf("%d", &pontosB04);
   return 0;
 }
